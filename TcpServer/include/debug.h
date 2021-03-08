7d1fd44dd4ad51db865997b125a1ea553b354c09
aqshing <asm.best@email>
2021-03-08 16:23:58 +0800

    TCP CS V0.2

--- TcpServer/include/debug.h
+++ TcpServer/include/debug.h
@@ -1,20 +1,24 @@
 #pragma once
-#ifndef _DEBUG_H_
-#define _DEBUG_H_
+//#ifndef _DEBUG_H_
+//#define _DEBUG_H_
 ///*************************调试输出宏**************************************///
 #if defined _DBG
 #define _DEBUGGING_
 #endif
 
 #ifdef _DEBUGGING_
+#ifdef __cplusplus
+#include <cstdio>
+#else
 #include <stdio.h>
+#endif
 #endif
 
 ///@brief 一般放于函数末尾, 判断CPU是否执行函数成功
 #ifdef _DEBUGGING_
-#define COUT() printf("%s: %d rows %s succeed\n", __FILE__, __LINE__, __func__)
+#define TAG() printf("%s: %d rows %s succeed\n", __FILE__, __LINE__, __func__)
 #else
-#define COUT()
+#define TAG()
 #endif
 
 ///@brief 打印变量的值
@@ -26,7 +30,7 @@
 #define MYDBG(name, value)
 #endif
 
-///@brief 功能等同与printf
+///@brief 功能等同与printf 为了兼容c89标准
 ///@brief SHOW(("num:%d", 0)); <=> printf("num:%d", 0);
 #ifdef _DEBUGGING_
 #define SHOW(args) printf args
@@ -34,12 +38,12 @@
 #define SHOW(args)
 #endif
 
-///@brief debug printf
-///@brief SHOW(("num:%d", 0)); <=> printf("num:%d", 0);
+///@brief debug output c99标准之后才支持
+///@brief COUT("num:%d", 0); <=> printf("num:%d", 0);
 #ifdef _DEBUGGING_
-#define DPRT(...) printf(__VA_ARGS)
+#define COUT(...) printf(__VA_ARGS__)
 #else
-#define DPRT(...)
+#define COUT(...)
 #endif
 
-#endif
+//#endif
